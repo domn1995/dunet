17769ab95b20c55e5b709256f5345fe2f44c1dba
Domn Werner <domn.werner@gmail.com>
2022-05-30 20:28:37 -0700

    Improve sample

--- samples/Dunet.Cli/Calculator.cs
+++ samples/Dunet.Cli/Calculator.cs
@@ -1,12 +1,12 @@
 ﻿namespace Dunet.Cli;
 
-public class AreaCalculator
+public class Calculator
 {
     public static double GetArea(IShape shape) => shape switch
     {
         Rectangle rect => rect.Length * rect.Width,
         Circle circle => 2.0 * Math.PI * circle.Radius,
         Triangle triangle => 1.0 / 2.0 * triangle.Base * triangle.Height,
-        _ => 0d
+        _ => 0d,
     };
 }
--- samples/Dunet.Cli/Program.cs
+++ samples/Dunet.Cli/Program.cs
@@ -3,7 +3,13 @@
 using Dunet.Cli;
 
 var rectangle = new Rectangle(10, 10);
+var triangle = new Triangle(10, 10);
+var circle = new Circle(10);
 
-var area = AreaCalculator.GetArea(rectangle);
+var rectangleArea = Calculator.GetArea(rectangle);
+var triangleArea = Calculator.GetArea(triangle);
+var circleArea = Calculator.GetArea(circle);
 
-Console.WriteLine(area);
+Console.WriteLine($"Rectangle area: {rectangleArea}");
+Console.WriteLine($"Triangle area: {triangleArea}");
+Console.WriteLine($"Circle area: {circleArea}");
