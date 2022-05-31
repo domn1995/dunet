4bd3af13d0792c457314ad8b29d2aedd4565e232
Domn Werner <domn.werner@gmail.com>
2022-05-30 19:32:10 -0700

    Create helper methods to simplify tests.

--- test/Dunet.Test/DiscriminatedUnionGeneratorTests.cs
+++ test/Dunet.Test/DiscriminatedUnionGeneratorTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
+using System.Collections.Immutable;
 using System.Reflection;
 
 namespace Dunet.Test;
@@ -9,7 +10,8 @@ public class DiscriminatedUnionGeneratorTests
     [Fact]
     public void Compiles()
     {
-        var compilation = CreateCompilation(@"
+        // Arrange.
+        var source = @"
 namespace Dunet.Cli;
 
 [Union]
@@ -18,29 +20,28 @@ public interface IShape
     IShape Circle(double radius);
     IShape Rectangle(double length, double width);
     IShape Triangle(double @base, double height);
-}
+}";
+        // Act.
+        var (output, diagnostics) = RunGenerator(source);
+        var outputDiagnostics = output?.GetDiagnostics();
 
-public static class AreaCalculator
-{
-    public static double GetArea(IShape shape) => shape switch
-    {
-        Rectangle rect => rect.Length * rect.Width,
-        Circle circle => 2.0 * System.Math.PI * circle.Radius,
-        Triangle triangle => 1.0 / 2.0 * triangle.Base * triangle.Height,
-        _ => 0d,
-    };
-}");
-        var generator = new DiscriminatedUnionGenerator();
-        CSharpGeneratorDriver.Create(generator)
-            .RunGeneratorsAndUpdateCompilation(compilation, out var outputCompilation, out var diagnostics);
-        var outputDiagnostics = outputCompilation.GetDiagnostics();
+        // Assert.
         Assert.Empty(diagnostics);
         Assert.Empty(outputDiagnostics);
     }
 
-    private static Compilation CreateCompilation(string source) =>
+    private static Compilation Compile(string source) =>
         CSharpCompilation.Create("compilation",
             new[] { CSharpSyntaxTree.ParseText(source) },
             new[] { MetadataReference.CreateFromFile(typeof(Binder).GetTypeInfo().Assembly.Location) },
             new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+    private static (Compilation? output, ImmutableArray<Diagnostic> diagnostics) RunGenerator(string source)
+    {
+        var compilation = Compile(source);
+        var generator = new DiscriminatedUnionGenerator();
+        CSharpGeneratorDriver.Create(generator)
+            .RunGeneratorsAndUpdateCompilation(compilation, out var outputCompilation, out var diagnostics);
+        return (outputCompilation, diagnostics);
+    }
 }
