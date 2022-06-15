f9683fc9c7da1654a041b8e14caab6bc98ebf792
Domn Werner <domn.werner@gmail.com>
2022-06-14 23:41:21 -0700

    File scoped namespace.

--- src/Dunet/AccessibilityExtensions.cs
+++ src/Dunet/AccessibilityExtensions.cs
@@ -1,22 +1,21 @@
 ﻿using Microsoft.CodeAnalysis;
 
-namespace Dunet
-{
-    internal static class AccessibilityExtensions
-    {
-        private static readonly Dictionary<Accessibility, string> accessibilityToKeywordMap =
-            new()
-            {
-                [Accessibility.Public] = "public",
-                [Accessibility.ProtectedOrInternal] = "protected internal",
-                [Accessibility.Internal] = "internal",
-                [Accessibility.Protected] = "protected",
-                [Accessibility.ProtectedAndInternal] = "private protected",
-                [Accessibility.Private] = "private",
-                [Accessibility.NotApplicable] = "",
-            };
+namespace Dunet;
 
-        public static string ToKeyword(this Accessibility accessibility) =>
-            accessibilityToKeywordMap[accessibility];
-    }
+internal static class AccessibilityExtensions
+{
+    private static readonly Dictionary<Accessibility, string> accessibilityToKeywordMap =
+        new()
+        {
+            [Accessibility.Public] = "public",
+            [Accessibility.ProtectedOrInternal] = "protected internal",
+            [Accessibility.Internal] = "internal",
+            [Accessibility.Protected] = "protected",
+            [Accessibility.ProtectedAndInternal] = "private protected",
+            [Accessibility.Private] = "private",
+            [Accessibility.NotApplicable] = "",
+        };
+
+    public static string ToKeyword(this Accessibility accessibility) =>
+        accessibilityToKeywordMap[accessibility];
 }
