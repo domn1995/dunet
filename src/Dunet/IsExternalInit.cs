0afb909fdb55e93d8e23374798921afa66c81804
Domn Werner <domn.werner@gmail.com>
2022-06-24 21:32:27 -0700

    Cleanup (#18)
    * Collapses a lot of complex loops into LINQ select statements and string joins.
    * Improve some names.

--- src/Dunet/IsExternalInit.cs
+++ src/Dunet/IsExternalInit.cs
@@ -1,4 +1,14 @@
-﻿namespace System.Runtime.CompilerServices
-{
-    internal static class IsExternalInit { }
-}
+﻿using System.ComponentModel;
+
+namespace System.Runtime.CompilerServices;
+
+/// <summary>
+/// Reserved to be used by the compiler for tracking metadata.
+/// This class should not be used by developers in source code.
+/// </summary>
+/// <remarks>
+/// Allows us to use records in a netstandard2.0 projects without getting the following error:
+/// Predefined type 'System.Runtime.CompilerServices.IsExternalInit' is not defined or imported
+/// </remarks>
+[EditorBrowsable(EditorBrowsableState.Never)]
+sealed class IsExternalInit { }
