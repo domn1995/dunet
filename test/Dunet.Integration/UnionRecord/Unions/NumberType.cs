bb5bf6d31d00ec4de573a4db3d660c33e425050b
Domn Werner <domn.werner@gmail.com>
2022-06-25 22:19:46 -0700

    Add union record integration tests

---
+++ test/Dunet.Integration/UnionRecord/Unions/NumberType.cs
@@ -0,0 +1,9 @@
+﻿namespace Dunet.Integration.UnionRecord.Unions;
+
+[Union]
+public partial record NumberType
+{
+    partial record Rational(int Numberator, int Denominator);
+
+    partial record Irrational(double Value);
+}
