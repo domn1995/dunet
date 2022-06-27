6bc45162d43e7656dd0fbe00ef2f45d286db5115
Domn Werner <domn.werner@gmail.com>
2022-06-26 20:22:48 -0700

    Add pokemon client sample.

---
+++ samples/PokemonClient/PokeClient.cs
@@ -0,0 +1,41 @@
+﻿using Dunet;
+using System.Net.Http.Json;
+
+namespace PokemonClient;
+
+// Some aliases to make code more terse and less noisy.
+using PokemonResult = Result<string, Pokemon>;
+using static Result<string, Pokemon>;
+
+public class PokeClient
+{
+    private readonly HttpClient client;
+
+    public PokeClient(HttpClient httpClient) => client = httpClient;
+
+    public async Task<PokemonResult> GetPokemonAsync(string name)
+    {
+        var fetch = () => client.GetFromJsonAsync<Pokemon>(name);
+
+        try
+        {
+            return await fetch() is Pokemon pokemon
+                ? new Success(pokemon)
+                : new Failure($"Unable to retrieve pokemon '{name}'.");
+        }
+        catch (Exception ex)
+        {
+            return new Failure(ex.Message);
+        }
+    }
+}
+
+public record Pokemon(int Id, string Name, int Weight, int Height);
+
+[Union]
+public partial record Result<TFailure, TSuccess>
+{
+    partial record Success(TSuccess Value);
+
+    partial record Failure(TFailure Error);
+}
