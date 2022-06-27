33067923b636a36f5631f0c2b46a7b5818e352d8
Domn Werner <domn.werner@gmail.com>
2022-06-26 19:35:27 -0700

    Add option monad sample.

---
+++ samples/OptionMonad/Program.cs
@@ -0,0 +1,34 @@
+﻿using Dunet;
+using static Option<int>;
+
+while (true)
+{
+    Console.Write("Please enter a number: ");
+    var input = ParseInt(Console.ReadLine());
+
+    var output = input.Match(
+        some => $"You entered the number: {some.Value}",
+        none => "That's not a number!"
+    );
+
+    Console.WriteLine(output);
+}
+
+static Option<int> ParseInt(string? value) =>
+    int.TryParse(value, out var num) ? new Some(num) : new None();
+
+[Union]
+public partial record Option<T>
+{
+    partial record Some(T Value)
+    {
+        public override bool IsSome => true;
+    }
+
+    partial record None()
+    {
+        public override bool IsSome => false;
+    }
+
+    public abstract bool IsSome { get; }
+}
