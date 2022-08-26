b78cf37679e6f51c4a654a507041d88eb09d400e
Domn Werner <domn.werner@gmail.com>
2022-08-25 22:44:22 -0700

    Add more tests for nested unions.

--- test/GenerateUnionRecord/NestedGenerationTests.cs
+++ test/GenerateUnionRecord/NestedGenerationTests.cs
@@ -86,4 +86,54 @@ public partial class Parent1
         result.CompilationErrors.Should().BeEmpty();
         result.GenerationDiagnostics.Should().BeEmpty();
     }
+
+    [Fact]
+    public void CanReturnDeeplyNestedMemberFromOtherNamespace()
+    {
+        var nestedCs =
+            @"
+using Dunet;
+
+namespace NestedTests;
+
+public partial class Parent1
+{
+    public partial class Parent2
+    {
+        public partial class Parent3
+        {
+            [Union]
+            public partial record Nested
+            {
+                public partial record Member1;
+                public partial record Member2;
+            }
+
+            public static Nested Foo()
+            {
+                return new Nested.Member1();
+            }
+
+            public static Nested Bar()
+            {
+                return new Nested.Member2();
+            }
+        }
+    }
+}";
+        // Arrange.
+        var programCs =
+            @"
+using NestedTests;
+
+var foo = Parent1.Parent2.Parent3.Foo();
+var bar = Parent1.Parent2.Parent3.Bar();
+";
+        // Act.
+        var result = Compile.ToAssembly(nestedCs, programCs);
+
+        // Assert.
+        result.CompilationErrors.Should().BeEmpty();
+        result.GenerationDiagnostics.Should().BeEmpty();
+    }
 }
