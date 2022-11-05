5de1fde8bc02e0f459ee1ce5c143ea9a0364afc3
Domn Werner <domn.werner@gmail.com>
2022-11-04 21:05:19 -0700

    Disable CS1591 in extensions (#76)
    * Release v1.4.1-preview2.

--- src/GenerateUnionExtensions/UnionExtensionsSource.cs
+++ src/GenerateUnionExtensions/UnionExtensionsSource.cs
@@ -35,7 +35,9 @@ internal static class UnionExtensionsSource
         builder.AppendLine(taskMethod);
         var valueTaskMethod = GenerateMatchAsyncMethod(union, "System.Threading.Tasks.ValueTask");
         builder.Append(valueTaskMethod);
-        builder.Append("}");
+        builder.AppendLine("}");
+
+        builder.AppendLine("#pragma warning restore 1591");
 
         return builder.ToString();
     }
@@ -87,8 +89,6 @@ internal static class UnionExtensionsSource
         }
 
         builder.AppendLine("        );");
-
-        builder.AppendLine("#pragma warning restore 1591");
 
         return builder.ToString();
     }
