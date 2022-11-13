0ea5c83333d44e32fb6bf9ebfb53dc029d8532e4
Domn Werner <domn.werner@gmail.com>
2022-11-13 13:54:50 -0800

    Async action match tests.

--- test/GenerateUnionExtensions/GenericMatchAsyncMethodTests.cs
+++ test/GenerateUnionExtensions/GenericMatchAsyncMethodTests.cs
@@ -56,6 +56,59 @@ async static Task<int> GetValueAsync() =>
     }
 
     [Theory]
+    [InlineData("Task", "new Option<int>.Some(1)", 1)]
+    [InlineData("ValueTask", "new Option<int>.Some(2)", 2)]
+    [InlineData("Task", "new Option<int>.None()", 0)]
+    [InlineData("ValueTask", "new Option<int>.None()", 0)]
+    public async Task GenericMatchAsyncCallsCorrectActionArgument(
+        string taskType,
+        string optionDeclaration,
+        int expectedValue
+    )
+    {
+        // Arrange.
+        const string optionCs =
+            @"
+using Dunet;
+
+namespace GenericsTest;
+
+[Union]
+partial record Option<T>
+{
+    partial record Some(T Value);
+    partial record None();
+}";
+
+        var programCs =
+            @$"
+using System.Threading.Tasks;
+using GenericsTest;
+
+async static {taskType}<Option<int>> GetOptionAsync()
+{{
+    await Task.Delay(0);
+    return {optionDeclaration};
+}}
+
+async static Task<int> GetValueAsync()
+{{
+    var value = 0;
+    await GetOptionAsync()
+        .MatchAsync(some => value = some.Value, none => value = 0);
+    return value;
+}}";
+        // Act.
+        var result = Compile.ToAssembly(optionCs, programCs);
+        var value = await result.Assembly!.ExecuteStaticAsyncMethod<int>("GetValueAsync");
+
+        // Assert.
+        result.CompilationErrors.Should().BeEmpty();
+        result.GenerationErrors.Should().BeEmpty();
+        value.Should().Be(expectedValue);
+    }
+
+    [Theory]
     [InlineData("Task", "new Result<string, double>.Success(1d)", "1")]
     [InlineData("ValueTask", "new Result<string, double>.Success(1d)", "1")]
     [InlineData("Task", "new Result<string, double>.Failure(\"Error!\")", "Error!")]
@@ -96,6 +149,61 @@ async static Task<string> GetValueAsync() =>
     await GetResultAsync()
         .MatchAsync(success => success.Value.ToString(), failure => failure.Error);
 ";
+
+        // Act.
+        var result = Compile.ToAssembly(resultCs, programCs);
+        var value = await result.Assembly!.ExecuteStaticAsyncMethod<string>("GetValueAsync");
+
+        // Assert.
+        result.CompilationErrors.Should().BeEmpty();
+        result.GenerationErrors.Should().BeEmpty();
+        value.Should().Be(expectedValue);
+    }
+
+    [Theory]
+    [InlineData("Task", "new Result<string, double>.Success(1d)", "1")]
+    [InlineData("ValueTask", "new Result<string, double>.Success(1d)", "1")]
+    [InlineData("Task", "new Result<string, double>.Failure(\"Error!\")", "Error!")]
+    [InlineData("ValueTask", "new Result<string, double>.Failure(\"Error!\")", "Error!")]
+    public async Task MultiGenericMatchAsyncCallsCorrectActionArgument(
+        string taskType,
+        string resultDeclaration,
+        string expectedValue
+    )
+    {
+        // Arrange.
+        const string resultCs =
+            @"
+using Dunet;
+
+namespace GenericsTest;
+
+[Union]
+partial record Result<TFailure, TSuccess>
+{
+    partial record Success(TSuccess Value);
+    partial record Failure(TFailure Error);
+}";
+
+        var programCs =
+            @$"
+using System;
+using System.Threading.Tasks;
+using GenericsTest;
+
+async static {taskType}<Result<string, double>> GetResultAsync()
+{{
+    await Task.Delay(0);
+    return {resultDeclaration};
+}}
+
+async static Task<string> GetValueAsync()
+{{
+    var value = """";
+    await GetResultAsync()
+        .MatchAsync(success => {{ value = success.Value.ToString(); }}, failure => {{ value = failure.Error; }});
+    return value;
+}}";
 
         // Act.
         var result = Compile.ToAssembly(resultCs, programCs);
