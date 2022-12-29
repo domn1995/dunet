fa2fbf45f3c82eb24f6e00e701cc1e3fd3a09b4c
Domn Werner <domn.werner@gmail.com>
2022-12-28 22:11:41 -0800

    Simplify identifier extensions.

--- src/IdentifierExtensions.cs
+++ src/IdentifierExtensions.cs
@@ -2,30 +2,16 @@
 
 internal static class IdentifierExtensions
 {
-    public static string ToMethodParameterCase(this string identifier)
-    {
-        if (identifier[0] is '@')
+    public static string ToMethodParameterCase(this string identifier) =>
+        identifier switch
         {
-            return identifier;
-        }
-
-        var isFirstCharacterLowercased = char.IsLower(identifier[0]);
-
-        // From here on we append "@" to the identifier name to prevent collisions with keywords.
-        // For example if `identifier` was "New", we'd get a collision when we lowered it to "new".
-        // Thus, we want "@new" instead. For simplicity and futureproofing, we don't append only on known keywords.
-        if (isFirstCharacterLowercased)
-        {
-            return $"@{identifier}";
-        }
-
-        var lowercasedFirstCharacter = char.ToLower(identifier[0]).ToString();
-
-        if (identifier.Length < 2)
-        {
-            return lowercasedFirstCharacter;
-        }
-
-        return $"@{lowercasedFirstCharacter}{identifier.Substring(1)}";
-    }
+            // If the identifier starts with '@', it's impossible to collide with a keyword, so we can just return it.
+            ['@', .. _] => identifier,
+            // If it's any other character:
+            // - Prepend '@' to prevent keyword conflicts.
+            // - Lowercase the first character to abide by C# style rules for method parameter casing.
+            [var firstCharacter, .. var rest] => $"@{char.ToLowerInvariant(firstCharacter)}{rest}",
+            // If an empty identifier came in, we just return it back and let the caller handle it.
+            { Length: 0 } => identifier,
+        };
 }
