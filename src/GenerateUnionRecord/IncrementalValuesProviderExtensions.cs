3be6d58d5daf753b6942d7ad9fc68832b3c655c3
Domn Werner <domn.werner@gmail.com>
2023-01-01 21:01:43 -0800

    Add incremental values provider extensions.

---
+++ src/GenerateUnionRecord/IncrementalValuesProviderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Dunet.GenerateUnionRecord;
+
+/// <summary>
+/// Provides extension methods for an <see cref="IncrementalValuesProvider{TValues}"/>.
+/// </summary>
+internal static class IncrementalValuesProviderExtensions
+{
+    /// <summary>
+    /// Removes <see langword="null"/> values from this provider.
+    /// </summary>
+    /// <typeparam name="T">The type of this provider's values.</typeparam>
+    /// <param name="provider">This incremental values provider.</param>
+    /// <returns>A new incremental values provider without <see langword="null"/> values.</returns>
+    public static IncrementalValuesProvider<T> Flatten<T>(
+        this IncrementalValuesProvider<T?> provider
+    ) => provider.Where(value => value is not null)!;
+}
