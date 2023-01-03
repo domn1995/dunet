acb7d06d40cbbd30b85fcdc965692ff87c001000
Domn Werner <domn.werner@gmail.com>
2023-01-02 16:58:45 -0800

    Prototype some struct based union types.

---
+++ samples/StructPrototype/Program.cs
@@ -0,0 +1,37 @@
+﻿using StructPrototype;
+
+while (true)
+{
+    Console.Write("Enter a shape (c, r, t): ");
+    var shapeInput = Console.ReadLine();
+    var shape = InputParser.ParseShape(shapeInput);
+    var area = shape.Match(Calculator.Area, () => 0);
+    Console.WriteLine($"Area = {area}");
+
+    Console.WriteLine("Enter a numerator: ");
+    var numeratorInput = Console.ReadLine();
+    Console.WriteLine("Enter a denominator: ");
+    var denominatorInput = Console.ReadLine();
+    var numerator = InputParser.ParseNumber<double>(numeratorInput);
+    var denominator = InputParser.ParseNumber<double>(denominatorInput);
+
+    // When you don't implement `SelectMany()` 😅
+    var result = numerator.Match(
+        err: numErr => numErr,
+        ok: top =>
+            denominator.Match(
+                err: denErr => denErr,
+                ok: bottom =>
+                    Calculator
+                        .Divide(top, bottom)
+                        .Match<Result<string, double>>(
+                            err: divErr => divErr.Message,
+                            ok: divOk => divOk
+                        )
+            )
+    );
+
+    var output = result.Match(err => $"Error: {err}", ok => ok.ToString());
+
+    Console.WriteLine(output);
+}
---
+++ samples/StructPrototype/Shape.cs
@@ -0,0 +1,89 @@
+﻿using System.Diagnostics;
+
+namespace StructPrototype;
+
+public record struct Shape
+{
+    public record struct Circle(double Radius);
+
+    public record struct Rectangle(double Height, double Width);
+
+    public record struct Triangle(double Base, double Height);
+
+    private enum ShapeType : byte
+    {
+        Circle,
+        Rectangle,
+        Triangle,
+    };
+
+    private ShapeType type;
+
+    private Circle circle;
+
+    public static Shape NewCircle(double radius) =>
+        new() { circle = new Circle(radius), type = ShapeType.Circle, };
+
+    public Circle UnwrapCircle() =>
+        type switch
+        {
+            ShapeType.Circle => circle,
+            var actual
+                => throw new InvalidOperationException(
+                    $"Expected {ShapeType.Circle} but got {actual}"
+                ),
+        };
+
+    private Rectangle rectangle;
+
+    public static Shape NewRectangle(double height, double width) =>
+        new() { rectangle = new Rectangle(height, width), type = ShapeType.Rectangle, };
+
+    public Rectangle UnwrapRectangle() =>
+        type switch
+        {
+            ShapeType.Rectangle => rectangle,
+            var actual
+                => throw new InvalidOperationException(
+                    $"Expected {ShapeType.Rectangle} but got {actual}"
+                ),
+        };
+
+    private Triangle triangle;
+
+    public static Shape NewTriangle(double @base, double height) =>
+        new() { triangle = new Triangle(@base, height), type = ShapeType.Triangle, };
+
+    public Triangle UnwrapTriangle() =>
+        type switch
+        {
+            ShapeType.Triangle => triangle,
+            var actual
+                => throw new InvalidOperationException(
+                    $"Expected {ShapeType.Triangle} but got {actual}"
+                ),
+        };
+
+    public TOut Match<TOut>(
+        Func<Circle, TOut> circle,
+        Func<Rectangle, TOut> rectangle,
+        Func<Triangle, TOut> triangle
+    ) =>
+        type switch
+        {
+            ShapeType.Circle => circle(this.circle),
+            ShapeType.Rectangle => rectangle(this.rectangle),
+            ShapeType.Triangle => triangle(this.triangle),
+            var invalid
+                => throw new UnreachableException($"Matched an unreachable union type: {invalid}"),
+        };
+}
+
+public static class ShapePrelude
+{
+    public static Shape Circle(double radius) => Shape.NewCircle(radius);
+
+    public static Shape Rectangle(double height, double width) => Shape.NewRectangle(height, width);
+
+    public static Shape Triangle(double @base, double height) => Shape.NewTriangle(@base, height);
+}
