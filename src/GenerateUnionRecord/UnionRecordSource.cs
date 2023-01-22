10feeb1acbb63454aa0f7cfb64f873bb97aa49fd
Domn Werner <domn.werner@gmail.com>
2023-01-22 12:44:34 -0800

    Factor our generation of abstract match methods.

--- src/GenerateUnionRecord/UnionRecordSource.cs
+++ src/GenerateUnionRecord/UnionRecordSource.cs
@@ -4,23 +4,23 @@ namespace Dunet.GenerateUnionRecord;
 
 internal static class UnionRecordSource
 {
-    public static string GenerateRecord(UnionRecord record)
+    public static string GenerateRecord(UnionRecord union)
     {
         var builder = new StringBuilder();
 
         builder.AppendLine("#pragma warning disable 1591");
 
-        foreach (var import in record.Imports)
+        foreach (var import in union.Imports)
         {
             builder.AppendLine(import);
         }
 
-        if (record.Namespace is not null)
+        if (union.Namespace is not null)
         {
-            builder.AppendLine($"namespace {record.Namespace};");
+            builder.AppendLine($"namespace {union.Namespace};");
         }
 
-        var parentTypes = record.ParentTypes;
+        var parentTypes = union.ParentTypes;
 
         foreach (var type in parentTypes)
         {
@@ -28,79 +28,27 @@ internal static class UnionRecordSource
             builder.AppendLine("{");
         }
 
-        builder.Append($"abstract partial record {record.Name}");
-        builder.AppendTypeParams(record.TypeParameters);
+        builder.Append($"abstract partial record {union.Name}");
+        builder.AppendTypeParams(union.TypeParameters);
         builder.AppendLine();
         builder.AppendLine("{");
-        builder.AppendLine($"    private {record.Name}() {{}}");
+        builder.AppendLine($"    private {union.Name}() {{}}");
         builder.AppendLine();
 
-        // Func match method.
-        builder.AppendLine("    public abstract TMatchOutput Match<TMatchOutput>(");
-        for (int i = 0; i < record.Members.Count; ++i)
-        {
-            var member = record.Members[i];
-            builder.Append($"        System.Func<{member.Identifier}");
-            builder.AppendTypeParams(member.TypeParameters);
-            builder.Append($", TMatchOutput> {member.Identifier.ToMethodParameterCase()}");
-            if (i < record.Members.Count - 1)
-            {
-                builder.Append(",");
-            }
-            builder.AppendLine();
-        }
-        builder.AppendLine("    );");
-
-        // Action match method.
-        builder.AppendLine("    public abstract void Match(");
-        for (int i = 0; i < record.Members.Count; ++i)
-        {
-            var member = record.Members[i];
-            builder.Append($"        System.Action<{member.Identifier}");
-            builder.AppendTypeParams(member.TypeParameters);
-            builder.Append($"> {member.Identifier.ToMethodParameterCase()}");
-            if (i < record.Members.Count - 1)
-            {
-                builder.Append(",");
-            }
-            builder.AppendLine();
-        }
-        builder.AppendLine("    );");
-
+        var abstractMatchMethods = GenerateAbstractMatchMethods(union);
+        builder.AppendLine(abstractMatchMethods);
         builder.AppendLine();
 
-        // Specific func match methods.
-        foreach (var member in record.Members)
-        {
-            builder.AppendLine(
-                $"    public abstract TMatchOutput Match{member.Identifier}<TMatchOutput>("
-            );
-            builder.Append($"        System.Func<{member.Identifier}");
-            builder.AppendTypeParams(member.TypeParameters);
-            builder.AppendLine($", TMatchOutput> {member.Identifier.ToMethodParameterCase()},");
-            builder.AppendLine($"        System.Func<TMatchOutput> @else");
-            builder.AppendLine("    );");
-        }
-
+        var abstractSpecificMatchMethods = GenerateAbstractSpecificMatchMethods(union);
+        builder.AppendLine(abstractSpecificMatchMethods);
         builder.AppendLine();
 
-        // Specific action match methods.
-        foreach (var member in record.Members)
+        if (SupportsImplicitConversions(union))
         {
-            builder.AppendLine($"    public abstract void Match{member.Identifier}(");
-            builder.Append($"        System.Action<{member.Identifier}");
-            builder.AppendTypeParams(member.TypeParameters);
-            builder.AppendLine($"> {member.Identifier.ToMethodParameterCase()},");
-            builder.AppendLine($"        System.Action @else");
-            builder.AppendLine("    );");
-        }
-
-        if (SupportsImplicitConversions(record))
-        {
-            foreach (var member in record.Members)
+            foreach (var member in union.Members)
             {
-                builder.Append($"    public static implicit operator {record.Name}");
-                builder.AppendTypeParams(record.TypeParameters);
+                builder.Append($"    public static implicit operator {union.Name}");
+                builder.AppendTypeParams(union.TypeParameters);
                 builder.AppendLine(
                     $"({member.Properties[0].Type.Identifier} value) => new {member.Identifier}(value);"
                 );
@@ -108,24 +56,24 @@ internal static class UnionRecordSource
             builder.AppendLine();
         }
 
-        foreach (var member in record.Members)
+        foreach (var member in union.Members)
         {
             builder.Append($"    public sealed partial record {member.Identifier}");
             builder.AppendTypeParams(member.TypeParameters);
-            builder.Append($" : {record.Name}");
-            builder.AppendTypeParams(record.TypeParameters);
+            builder.Append($" : {union.Name}");
+            builder.AppendTypeParams(union.TypeParameters);
             builder.AppendLine();
             builder.AppendLine("    {");
 
             // Func match method.
             builder.AppendLine("        public override TMatchOutput Match<TMatchOutput>(");
-            for (int i = 0; i < record.Members.Count; ++i)
+            for (int i = 0; i < union.Members.Count; ++i)
             {
-                var memberParam = record.Members[i];
+                var memberParam = union.Members[i];
                 builder.Append($"            System.Func<{memberParam.Identifier}");
                 builder.AppendTypeParams(memberParam.TypeParameters);
                 builder.Append($", TMatchOutput> {memberParam.Identifier.ToMethodParameterCase()}");
-                if (i < record.Members.Count - 1)
+                if (i < union.Members.Count - 1)
                 {
                     builder.Append(",");
                 }
@@ -135,13 +83,13 @@ internal static class UnionRecordSource
 
             // Action match method.
             builder.AppendLine("        public override void Match(");
-            for (int i = 0; i < record.Members.Count; ++i)
+            for (int i = 0; i < union.Members.Count; ++i)
             {
-                var memberParam = record.Members[i];
+                var memberParam = union.Members[i];
                 builder.Append($"            System.Action<{memberParam.Identifier}");
                 builder.AppendTypeParams(memberParam.TypeParameters);
                 builder.Append($"> {memberParam.Identifier.ToMethodParameterCase()}");
-                if (i < record.Members.Count - 1)
+                if (i < union.Members.Count - 1)
                 {
                     builder.Append(",");
                 }
@@ -150,7 +98,7 @@ internal static class UnionRecordSource
             builder.AppendLine($"        ) => {member.Identifier.ToMethodParameterCase()}(this);");
 
             // Specific func match methods.
-            foreach (var specificMember in record.Members)
+            foreach (var specificMember in union.Members)
             {
                 builder.AppendLine(
                     $"        public override TMatchOutput Match{specificMember.Identifier}<TMatchOutput>("
@@ -175,7 +123,7 @@ internal static class UnionRecordSource
             }
 
             // Specific action match methods.
-            foreach (var specificMember in record.Members)
+            foreach (var specificMember in union.Members)
             {
                 builder.AppendLine(
                     $"        public override void Match{specificMember.Identifier}("
@@ -236,5 +184,73 @@ internal static class UnionRecordSource
         return membersHaveSingleParameter()
             && membersHaveNoInterfaceParameters()
             && membersHaveUniqueParameterTypes();
+    }
+
+    private static string GenerateAbstractMatchMethods(UnionRecord union)
+    {
+        var builder = new StringBuilder();
+        builder.AppendLine("    public abstract TMatchOutput Match<TMatchOutput>(");
+        for (int i = 0; i < union.Members.Count; ++i)
+        {
+            var member = union.Members[i];
+            builder.Append($"        System.Func<{member.Identifier}");
+            builder.AppendTypeParams(member.TypeParameters);
+            builder.Append($", TMatchOutput> {member.Identifier.ToMethodParameterCase()}");
+            if (i < union.Members.Count - 1)
+            {
+                builder.Append(",");
+            }
+            builder.AppendLine();
+        }
+        builder.AppendLine("    );");
+
+        // Action match method.
+        builder.AppendLine("    public abstract void Match(");
+        for (int i = 0; i < union.Members.Count; ++i)
+        {
+            var member = union.Members[i];
+            builder.Append($"        System.Action<{member.Identifier}");
+            builder.AppendTypeParams(member.TypeParameters);
+            builder.Append($"> {member.Identifier.ToMethodParameterCase()}");
+            if (i < union.Members.Count - 1)
+            {
+                builder.Append(",");
+            }
+            builder.AppendLine();
+        }
+        builder.AppendLine("    );");
+
+        return builder.ToString();
+    }
+
+    private static string GenerateAbstractSpecificMatchMethods(UnionRecord union)
+    {
+        var builder = new StringBuilder();
+
+        foreach (var member in union.Members)
+        {
+            builder.AppendLine(
+                $"    public abstract TMatchOutput Match{member.Identifier}<TMatchOutput>("
+            );
+            builder.Append($"        System.Func<{member.Identifier}");
+            builder.AppendTypeParams(member.TypeParameters);
+            builder.AppendLine($", TMatchOutput> {member.Identifier.ToMethodParameterCase()},");
+            builder.AppendLine($"        System.Func<TMatchOutput> @else");
+            builder.AppendLine("    );");
+        }
+
+        builder.AppendLine();
+
+        foreach (var member in union.Members)
+        {
+            builder.AppendLine($"    public abstract void Match{member.Identifier}(");
+            builder.Append($"        System.Action<{member.Identifier}");
+            builder.AppendTypeParams(member.TypeParameters);
+            builder.AppendLine($"> {member.Identifier.ToMethodParameterCase()},");
+            builder.AppendLine($"        System.Action @else");
+            builder.AppendLine("    );");
+        }
+
+        return builder.ToString();
     }
 }
