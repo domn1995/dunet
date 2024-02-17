c712e624c9c30435b5effaffb7106efe0e2ab757
Domn Werner <domn.werner@gmail.com>
2024-02-16 20:16:48 -0800

    Update result type.

--- samples/StructPrototype/Result.cs
+++ samples/StructPrototype/Result.cs
@@ -9,7 +9,7 @@ public partial record struct Result<TErr, TOk>
     public record struct Err(TErr Error);
 }
 
-public partial record struct Result<TErr, TOk>
+public readonly partial record struct Result<TErr, TOk>
 {
     private enum ResultType : byte
     {
@@ -17,14 +17,18 @@ public partial record struct Result<TErr, TOk>
         Ok,
     };
 
-    private ResultType type;
+    private readonly ResultType type;
 
-    private Ok ok;
-    private Err err;
+    private readonly Ok ok;
+    private readonly Err err;
 
-    public static implicit operator Result<TErr, TOk>(TOk value) => Prelude.Ok(value);
+    public static implicit operator Result<TErr, TOk>(TOk value) => NewOk(value);
 
-    public static implicit operator Result<TErr, TOk>(TErr error) => Prelude.Err(error);
+    public static implicit operator Result<TErr, TOk>(TErr error) => NewErr(error);
+
+    private Result(ResultType type, Ok ok) => (this.type, this.ok) = (type, ok);
+
+    private Result(ResultType type, Err err) => (this.type, this.err) = (type, err);
 
     public TOut Match<TOut>(Func<Err, TOut> err, Func<Ok, TOut> ok) =>
         type switch
@@ -37,20 +41,7 @@ public partial record struct Result<TErr, TOk>
                 ),
         };
 
-    public static class Prelude
-    {
-        public static Result<TErr, TOk> Err(TErr err) =>
-            new() { type = ResultType.Err, err = new Err(err) };
+    public static Result<TErr, TOk> NewOk(TOk value) => new(ResultType.Ok, new Ok(value));
 
-        public static Result<TErr, TOk> Ok(TOk value) =>
-            new() { type = ResultType.Ok, ok = new Ok(value) };
-    }
-}
-
-public static class ResultPrelude
-{
-    public static Result<TErr, TOk> Ok<TErr, TOk>(TOk value) => Result<TErr, TOk>.Prelude.Ok(value);
-
-    public static Result<TErr, TOk> Err<TErr, TOk>(TErr error) =>
-        Result<TErr, TOk>.Prelude.Err(error);
+    public static Result<TErr, TOk> NewErr(TErr error) => new(ResultType.Ok, new Err(error));
 }
