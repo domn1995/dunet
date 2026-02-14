534b327c2b14fdadfaa2d858cf336d1c7d068cd1
Domn Werner <domn.werner@gmail.com>
2026-02-14 15:02:25 -0800

    feat: disable implicit conversions

--- src/Dunet/UnionAttribute.cs
+++ src/Dunet/UnionAttribute.cs
@@ -4,4 +4,18 @@
 /// Enables dunet union source generation for the decorated partial record.
 /// </summary>
 [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
-public sealed class UnionAttribute : Attribute { }
+public sealed class UnionAttribute : Attribute
+{
+    /// <summary>
+    /// <para>
+    /// Gets or initializes whether the union should support the direct assignment of each
+    /// variant's underlying type directly to the union type.
+    /// </para>
+    /// <para>Defaults to <c>true</c>.</para>
+    /// <para>
+    /// If this is enabled, the generator will only create implicit conversions if each variant has
+    /// exactly zero or one parameter and the parameter type is not an interface.
+    /// </para>
+    /// </summary>
+    public bool EnableImplicitConversions { get; init; } = true;
+}
